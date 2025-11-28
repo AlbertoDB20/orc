23ed600a1ac2439866362b66c8ad1f3c7df3248a
AlbertoDB20 <alberto.dalbosco@studenti.unitn.it>
2025-11-28 13:38:36 +0100

    lecture 28 11

--- RL/sol/ex_4_sarsa_sol.py
+++ RL/sol/ex_4_sarsa_sol.py
@@ -32,15 +32,40 @@ def sarsa(env, gamma, Q, nEpisodes, maxEpisodeLength,
     '''
     h_ctg = []           # Learning history (for plots)
     # for every episode
-    # reset the environment state
-    # compute u with an epsilon-greedy policy
-    # for every step of the episode
-    # compute u_next with an epsilon-greedy policy
-    # compute Q target
-    # update Q function with TD
-    # update x, u with x_next, u_next
-    # update epsilon with exponentially decaying function: eps=e^(-decay*episode)
-    # append cost-to-go to list h_ctg (for plots)
-    # every nprint episodes print mean V, mean cost-to-go, and epsilon
+    for ep in range (nEpisodes):
+        # reset the environment state
+        x = env.reset()
+        # compute u with an epsilon-greedy policy
+        u = epsGreedy(Q, x, eps, env.nu)
+        # initialize cost-to-go for this episode
+        J = 0.0
+        # for every step of the episode
+        for t in range (maxEpisodeLength):
+            # take action u, observe x_next, c
+            x_next, cost = env.step(u)
+            # compute u_next with an epsilon-greedy policy
+            u_next = epsGreedy(Q, x_next, eps, env.nu)
+            # compute Q target
+            Q_target = cost + gamma * Q[x_next, u_next]
+            # update Q function with TD
+            Q[x,u] += learningRate * (Q_target - Q[x,u])
+            # update x, u with x_next, u_next
+            x, u = x_next, u_next
+            # update cost-to-go for this episode
+            J += (gamma**t) * cost
+
+        # update epsilon with exponentially decaying function: eps=e^(-decay*episode)
+        eps = np.exp(-eps_decreasing_decay*ep)
+        eps = max(eps, min_eps)  #saturate epsilon if it is below min_eps
+
+        # append cost-to-go to list h_ctg (for plots)
+        h_ctg.append(J)
+        # every nprint episodes print mean V, mean cost-to-go, and epsilon
+        if (ep % nprint == 0):
+            V, pi = compute_V_pi_from_Q(env, Q)
+            print("Episode %d/%d - Mean V: %.2f - Mean cost-to-go: %.2f - Epsilon: %.4f" 
+                  % (ep, nEpisodes, np.mean(V), np.mean(h_ctg[-nprint:]), eps*100))
+            if plot:
+                env.plot_V(V, ep)
         
     return Q, h_ctg
--- RL/sol/ex_5_q_learning_sol.py
+++ RL/sol/ex_5_q_learning_sol.py
@@ -8,6 +8,11 @@ Created on Mon Nov 29 23:56:07 2021
 import numpy as np
 from numpy.random import randint, uniform
 
+def Greedy(Q, x, exploration_prob, nu):
+    if uniform(0,1) < exploration_prob:
+        return randint(nu)
+    return np.argmin(Q[x,:])
+
 def q_learning(env, gamma, Q, nEpisodes, maxEpisodeLength, 
                learningRate, exploration_prob, exploration_decreasing_decay,
                min_exploration_prob, compute_V_pi_from_Q, plot=False, nprint=1000):
@@ -28,16 +33,39 @@ def q_learning(env, gamma, Q, nEpisodes, maxEpisodeLength,
     # Keep track of the cost-to-go history (for plot)
     h_ctg = []
     # Make a copy of the initial Q table guess
+    Q = Q.copy()
     # for every episode
-    # reset the state
-    # simulate the system for maxEpisodeLength steps
-    # with probability exploration_prob take a random control input
-    # otherwise take a greedy control
-    # Compute reference Q-value at state x
-    # Update Q-Table with the given learningRate
-    # keep track of the cost to go
-    # update epsilon with an exponential decay: eps = exp(-decay*episode)
-    
-    # use the function compute_V_pi_from_Q(env, Q) to compute and plot V and pi
+    for ep in range (nEpisodes):
+        # reset the state
+        x = env.reset()
+        # initialize cost-to-go for this episode
+        cost_to_go = 0.0
+        # simulate the system for maxEpisodeLength steps
+        for t in range (maxEpisodeLength):
+            # with probability exploration_prob take a random control input
+            u = Greedy(Q, x, exploration_prob, env.nu)
+            # otherwise take a greedy control
+            x_next, cost = env.step(u)
+            # accumulate cost-to-go
+            cost_to_go += (gamma**t) * cost
+            # Compute reference Q-value at state x
+            Q_target = cost + gamma * min(Q[x_next, :])
+            # Update Q-Table with the given learningRate
+            Q[x, u] += learningRate * (Q_target - Q[x, u])
+            # update state
+            x = x_next
+
+        # update epsilon with an exponential decay: eps = exp(-decay*episode)
+        exploration_prob = np.exp(-exploration_decreasing_decay*ep)
+        exploration_prob = max(exploration_prob, min_exploration_prob)  #saturate epsilon if it is below min_eps
+        # keep track of the cost to go
+        h_ctg.append(cost_to_go)
+        # use the function compute_V_pi_from_Q(env, Q) to compute and plot V and pi
+        if(plot and (ep % nprint == 0)):
+            V, pi = compute_V_pi_from_Q(env, Q)
+            env.plot_V_table(V)
+            env.plot_policy(pi)
+            print("Episode %d/%d - Average/min/max Value: %.2f / %.2f / %.2f" % 
+                  (ep, nEpisodes, np.mean(V), np.min(V), np.max(V)) )
     
     return Q, h_ctg
